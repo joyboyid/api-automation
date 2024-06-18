6f4a3e165455e4270705cf1120a8b6aedcc4f3d5
joyboyid <ican.notfound@gmail.com>
2024-06-18 07:14:11 +0700

    assert

--- test/api_test.js
+++ test/api_test.js
@@ -1,12 +1,19 @@
+import  { expect, assert, should } = require("chai");
 const supertest = require("supertest");
+
 const baseurl = "https://reqres.in";
 
 describe('Req res teseting', () => {
     it('GET', async() => {
         const response = await supertest(baseurl)
         .get("/api/users/2");
-        console.log(response.status);
-        console.log(response.body);
+        // console.log(response.status);
+        // console.log(response.body);
+        expect(response.status).to.equal(200)
+        expect(response.body[0].id).to.equal('2')
+        expect(response.body[0].first_name).to.equal('Janet')
+        expect(response.body[0].last_name).to.equal('Weaver')
+        expect(response.body[0].email).to.equal('janet.weaver@reqres.in')    
     });
     it('POST', async() => {
         const body ={
